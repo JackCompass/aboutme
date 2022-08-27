84d224500da8433de46210c6175ed49d3c77944b
Anuj <kumaranuj1303@gmail.com>
2022-08-27 10:00:20 +0530

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ const Document = () => {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
                 <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&display=swap"
                       rel="stylesheet"/>
+                <link rel={"icon"} type="image/x-icon" href={"/favicon.svg"}/>
             </Head>
             <body>
             <Main/>
