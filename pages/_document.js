bf8a5754586a64ef24fea2e8f4e92f26f5ec643c
Anuj <kumaranuj1303@gmail.com>
2022-08-29 10:51:08 +0530

    set title to author name

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ const Document = () => {
     return (
         <Html>
             <Head>
+                <title>Anuj Singh</title>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
                 <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&display=swap"
