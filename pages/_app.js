bcafdfa346feb9e9be1ff897bd5d6f0e7bdbe6c4
anuj <kumaranuj1303@gmail.com>
2024-09-20 13:14:23 +0530

    fix: warnings on server start
    warnings fixed:
    1. don't use title inside the _doucment.js
    2. use crossOrigin="true" instead of crossOrigin.

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,22 @@ import "../styles/globals.css";
 import Error from "../components/error/Error";
 import { RecoilRoot } from "recoil";
 import { SkeletonTheme } from "react-loading-skeleton";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <RecoilRoot>
-      <Error>
-        <SkeletonTheme duration={0.8}>
-          <Component {...pageProps} />
-        </SkeletonTheme>
-      </Error>
-    </RecoilRoot>
+    <>
+      <Head>
+        <title>Anuj Singh</title>
+      </Head>
+      <RecoilRoot>
+        <Error>
+          <SkeletonTheme duration={0.8}>
+            <Component {...pageProps} />
+          </SkeletonTheme>
+        </Error>
+      </RecoilRoot>
+    </>
   );
 }
 
